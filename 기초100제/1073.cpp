d0e09c13ce52c2bdb5ab78f57162bf2a6b05729f
Simu96 <62987451+Simu96@users.noreply.github.com>
2021-03-16 11:28:15 +0900

    Create 1073.cpp

---
+++ 기초100제/1073.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int a;
+    while(1){
+        scanf("%d", &a);
+        if(a == 0){
+            break;
+        }else{
+        printf("%d\n", a);
+        }
+    }
+}
+
